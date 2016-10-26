a8c7de76360bf028d2840af2e0c7f57a6672fb43
jeff stringer <jeff@hubbubhealth.com>
2016-10-25 19:07:01 -0700

    add company, recall relationship

--- app/models/recall.rb
+++ app/models/recall.rb
@@ -2,4 +2,6 @@ class Recall < ActiveRecord::Base
 
   validates_presence_of :company_id, :product_description, :reason, :company_release_link, :release_date
   validates_uniqueness_of :company_release_link
+
+  belongs_to :company
 end
