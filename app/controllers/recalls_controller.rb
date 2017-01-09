e072869f1dbba9200468444322090e7ede9c0846
jeff stringer <jeff@hubbubhealth.com>
2017-01-08 18:50:08 -0800

    add strong params to recalls_controller

--- app/controllers/recalls_controller.rb
+++ app/controllers/recalls_controller.rb
@@ -1,5 +1,5 @@
 class RecallsController < ApplicationController
-  skip_before_filter :verify_authenticity_token
+  skip_before_action :verify_authenticity_token
 
   def index
     @recalls = Recall.where(release_date: 6.month.ago..Date.today).order(release_date: :desc).limit(10)
@@ -10,7 +10,22 @@ class RecallsController < ApplicationController
   end
 
   def create
-    DataImporter.new(params[:_json])
+    DataImporter.new(recalls_params)
     redirect_to root_path
+  end
+
+  private
+
+  def recalls_params
+    params.require(:_json).map do |json|
+      json.require(:recall).
+        permit( :release_date,
+                :brand_name,
+                :product_description,
+                :reason,
+                :name,
+                :company_release_link
+              )
+    end
   end
 end
