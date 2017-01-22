8f932d89112b2acaef9950a1a7f9c999a03cf13a
jeff stringer <jeff@hubbubhealth.com>
2017-01-21 19:42:28 -0800

    fix error in views

--- app/models/recall.rb
+++ app/models/recall.rb
@@ -9,12 +9,10 @@ class Recall < ApplicationRecord
     def bulk_import(payload)
       recalls = []
       payload.each do |recall|
-        company = Company.find_by(name: recall["brand_name"])
+        company = Company.find_or_create_by(name: recall["brand_name"])
         existing_recall = Recall.find_by(company_release_link: recall["company_release_link"])
         unless existing_recall
-          recalls << company.recalls.build(
-            recall.slice("release_date","product_description","reason","company_release_link")
-          )
+          recalls << company.recalls.build(recall.slice("release_date","product_description","reason","company_release_link"))
         end
       end
       Recall.import recalls
