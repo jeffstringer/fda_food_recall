e90ea9a86a2bc94c73e54fe24461723b591be4e5
jeff stringer <jeff@hubbubhealth.com>
2017-01-21 19:26:07 -0800

    add recalls#index spec

--- spec/controllers/recalls_controller_spec.rb
+++ spec/controllers/recalls_controller_spec.rb
@@ -2,6 +2,15 @@ require 'spec_helper'
 include FdaResponseData
 
 RSpec.describe RecallsController, :type => :controller  do
+  describe "#index" do
+    it 'retrieves companies and recalls' do
+      company = FactoryGirl.create(:company)
+      recall = FactoryGirl.create(:recall, company: company)
+      get :index
+      expect(response.status).to eq(200)
+    end
+  end
+
   describe "#create" do
     it 'redirects to index page' do
       post :create, params: { _json: [recall_json, recall_json] }
