415e823c8dcab8fb8326722b1f259759d119b306
jeff stringer <jeff@hubbubhealth.com>
2017-02-01 16:56:08 -0800

    modify @recalls in recalls#index

--- app/controllers/recalls_controller.rb
+++ app/controllers/recalls_controller.rb
@@ -2,7 +2,7 @@ class RecallsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-    @recalls = Recall.where(release_date: 6.month.ago..Date.today).order(release_date: :desc).limit(10)
+    @recalls = Recall.order(release_date: :desc).limit(10)
   end
 
   def show
